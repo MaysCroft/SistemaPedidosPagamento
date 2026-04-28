cbaf05fe0a54b5f1d7a3850dd1d065d0acaedab9
May_Croft <mayconpilar19@gmail.com>
2026-04-27 21:33:57 -0300

    Add project files.

---
+++ Shared/PedidosData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Shared
+{
+    public class PedidosData
+    {
+        public int Id { get; set; }
+        public DateTime Data_Pedido { get; set; }
+        public string Nome_Cliente { get; set; }
+        public string Doc_Cliente { get; set; }
+        public string Itens { get; set; }
+        public double Valor { get; set; }
+        public string Status { get; set; }
+        public string Pagamento { get; set; }
+    }
+}
