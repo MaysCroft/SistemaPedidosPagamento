06a496ab463405cab1f27e8128547b97b7617e18
May_Croft <mayconpilar19@gmail.com>
2026-04-28 19:57:45 -0300

    Criação e implementação da RelayCommand

---
+++ PedidoInterface/Commands/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace PedidoInterface.Commands
+{
+    internal class RelayCommand : ICommand
+    {
+        private readonly Action execute;
+        private readonly Func<bool> canExecute;
+
+        public RelayCommand(Action execute, Func<bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            execute();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || canExecute();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
